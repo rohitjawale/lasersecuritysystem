1666f4219e186734276fa0ec95e3dd727feacff5
Rohit Jawale <32890620+rohitjawale@users.noreply.github.com>
2020-08-09 10:48:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+API_KEY = "b42e73ab-50fd-4bb6-9a9c-68028f20cc63"
+DEVICE_ID = "BOLT3851773"
+
+telegram_chat_id = "@scoeproject"
+telegram_bot_id = "bot1206290761:AAH6C1GbVDlFosW9dQs2X8d2QSzwzEsT5rs"
